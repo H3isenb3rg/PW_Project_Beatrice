d7bb6f46972d1d1b99ae41c20f4523179255d368
Matteo Beatrice <matteoe97@gmail.com>
2022-06-15 19:45:30 +0200

    Small improvement to well collapse animation

--- project_beatrice/public/js/event_well.js
+++ project_beatrice/public/js/event_well.js
@@ -1,11 +1,21 @@
-function down_caret(span_id) {
+function hidden_caret(span_id) {
     $(span_id).removeClass();
     $(span_id).addClass("bi bi-caret-down-fill");
 }
 
-function up_caret(span_id) {
+function shown_caret(span_id) {
     $(span_id).removeClass();
     $(span_id).addClass("bi bi-caret-up-fill");
+}
+
+function hide_caret(span_id) {
+    $(span_id).removeClass();
+    $(span_id).addClass("bi bi-arrow-up-short");
+}
+
+function show_caret(span_id) {
+    $(span_id).removeClass();
+    $(span_id).addClass("bi bi-arrow-down-short");
 }
 
 $(document).ready(function() {
@@ -13,11 +23,19 @@ $(document).ready(function() {
         var collapsed_panel = this;
         
         $("#"+this.id).on('show.bs.collapse', (function(id) {
-            return function() {up_caret(id)}
+            return function() {show_caret(id)}
         })("#icon"+this.id))
 
         $("#"+this.id).on('hide.bs.collapse', (function(id) {
-            return function() {down_caret(id)}
+            return function() {hide_caret(id)}
+        })("#icon"+this.id))
+        
+        $("#"+this.id).on('shown.bs.collapse', (function(id) {
+            return function() {shown_caret(id)}
+        })("#icon"+this.id))
+
+        $("#"+this.id).on('hidden.bs.collapse', (function(id) {
+            return function() {hidden_caret(id)}
         })("#icon"+this.id))
     })
 });
